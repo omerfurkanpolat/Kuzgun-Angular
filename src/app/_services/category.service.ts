02397499e4d3ec4025b0c846a39e9aca9b44c185
Ömer Furkan Polat <talopnakruf@outlook.com>
2021-01-06 14:23:17 +0300

    first commit

---
+++ src/app/_services/category.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
+import { Category } from '../_models/categories';
+
+@Injectable({
+    providedIn:"root"
+})
+
+export class CategoryService{
+    baseUrl:string="https://localhost:44386/api/admin"; 
+    baseUrl2:string="https://localhost:44386/api/home"
+   constructor(private http:HttpClient){
+
+   }
+   
+
+   getCategories():Observable<Category[]>{
+       return this.http.get<Category[]>(this.baseUrl2+'/getactivecategories');
+
+   }
+   deleteCategory(id:number){
+       return this.http.delete(this.baseUrl+'/deleteCategory/'+ id);
+   }
+   addCategory(model:any){
+      return this.http.post(this.baseUrl+ '/createCategory', model);
+   }
+
+   getCategory(id:number):Observable<Category>{
+       return this.http.get<Category>(this.baseUrl+'/getCategory/'+id);
+   }
+
+   updateCategory(id:number, category:Category){
+        return this.http.put(this.baseUrl+'/updateCategory/'+id, category)
+   }
+}
